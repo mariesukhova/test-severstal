7721483bfdece75d6adef9a848faf0cc80fbad8b
Maria Sukhova <mariesukhova@gmail.com>
2023-06-25 19:35:11 +0300

    add post page

---
+++ src/pages/post/PostPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PageContent from '../../components/PageContent/PageContent'
+
+function PostPage (): JSX.Element {
+  return <PageContent path="/post_post" tab="post" />
+}
+
+export default PostPage
