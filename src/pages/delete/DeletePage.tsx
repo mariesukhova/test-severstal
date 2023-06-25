c0f05cc012a27b6e8aef727dd2c6e6368b21ae4a
Maria Sukhova <mariesukhova@gmail.com>
2023-06-25 19:34:45 +0300

    add delete page

---
+++ src/pages/delete/DeletePage.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import PageContent from '../../components/PageContent/PageContent'
+
+export default function DeletePage (): JSX.Element {
+  return <PageContent path="/delete_delete" tab="delete" />
+}
