246b6bbeedf7857bcf0bfe45452a23dedda246be
Maria Sukhova <mariesukhova@gmail.com>
2023-07-10 17:57:01 +0300

    move pages into app.tsx

--- src/pages/get/GetPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import PageContent from '../../components/PageContent/PageContent'
-
-export default function GetPage (): JSX.Element {
-  return <PageContent path="/get_get" tab="get" />
-}
--- src/pages/post/PostPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PageContent from '../../components/PageContent/PageContent'
-
-function PostPage (): JSX.Element {
-  return <PageContent path="/post_post" tab="post" />
-}
-
-export default PostPage
